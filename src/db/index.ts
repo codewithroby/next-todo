91796a2b7d92290018a703c61945539099d96889
Roby Codes <robert.cfx@proton.me>
2024-08-23 16:14:14 +0200

    fix(index): Removed migrate function from main database endpoint.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,8 @@
 import postgres from "postgres";
 
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 import { env } from "~/env";
 
-const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
-migrate(drizzle(migrationClient), { migrationsFolder: "./" });
-
 const queryClient = postgres(env.DATABASE_URL);
-const db = drizzle(queryClient);
+export const db = drizzle(queryClient);
