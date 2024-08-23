1a6c1dabd34e898e276962af2a3ff73cd3babf28
Roby Codes <robert.cfx@proton.me>
2024-08-24 00:54:03 +0200

    refactor(drizzle-config.ts): Moved dialect value above.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "~/env";
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./src/db/output",
-  dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
