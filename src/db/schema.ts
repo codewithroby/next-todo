9b13b41a62143d6e575bd48b90ad762baeabbe6f
Roby Codes <robert.cfx@proton.me>
2024-08-23 15:57:24 +0200

    style(schema): Removed unused imports.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,9 +8,13 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const todos = pgTable("todos", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   title: varchar("title", { length: 256 }),
   description: text("description"),
   completed: boolean("completed").default(false),
   completedAt: timestamp("completed_at").defaultNow(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
