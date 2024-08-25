0687c94006d50258859c3a6aef688cde82b4f454
Roby Codes <robert.cfx@proton.me>
2024-08-25 03:15:40 +0200

    feat(todos): Added NoteBook component.

---
+++ src/components/todos/NoteBook.tsx
@@ -0,0 +1,7 @@
+const NoteBook = () => (
+  <div className="relative h-[500px] w-full max-w-md rounded-xl bg-black p-4">
+    <div className="size-full rounded-md bg-yellow-50"></div>
+  </div>
+);
+
+export { NoteBook };
