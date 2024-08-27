19ebc205d320c661339f4919924a19001d034349
Roby Codes <robert.cfx@proton.me>
2024-08-28 00:15:40 +0200

    style(page): Removed unused items from html.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,22 +102,6 @@ const HomePage = () => (
                 Add To-Do
               </Link>
             </nav>
-            <div className="mt-auto">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Upgrade to Pro</CardTitle>
-                  <CardDescription>
-                    Unlock all features and get unlimited access to our support
-                    team.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <Button size="sm" className="w-full">
-                    Upgrade
-                  </Button>
-                </CardContent>
-              </Card>
-            </div>
           </SheetContent>
         </Sheet>
       </header>
