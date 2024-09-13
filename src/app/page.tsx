4b1d0479def922a013ee6acaf350edeaef77ca8d
Roby Codes <robert.cfx@proton.me>
2024-09-13 20:51:41 +0200

    Completed HomePage Demo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-const HomePage = () => <div></div>;
+import { TodosList } from "~/components/todo-list";
+
+const HomePage = () => (
+  <main>
+    <section className="flex h-[100dvh] flex-col items-center justify-center gap-8">
+      <h1 className="handwritten text-4xl font-semibold uppercase">
+        Next TODO
+      </h1>
+      <TodosList />
+    </section>
+  </main>
+);
 
 export default HomePage;
