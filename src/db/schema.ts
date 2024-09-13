ca12531ef7eee73d27056b6c0e011616116c297b
Roby Codes <robert.cfx@proton.me>
2024-09-13 21:36:59 +0200

    schema(todos): Updated table schema.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -10,7 +10,7 @@ import {
 
 export const todos = pgTable("todos", {
   id: uuid("id").primaryKey().defaultRandom(),
-  pid: serial("pagination_id"),
+  pagination_id: serial("pagination_id"),
   title: varchar("title", { length: 256 }),
   description: text("description"),
   completed: boolean("completed").default(false),
