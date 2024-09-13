58fd73d9610e1488ae59564e27ba5ae026bf9c80
Roby Codes <robert.cfx@proton.me>
2024-09-13 20:54:24 +0200

    style(home): added padding on top and bottom and removed fixed height of main section for responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { TodosList } from "~/components/todo-list";
 
 const HomePage = () => (
   <main>
-    <section className="flex h-[100dvh] flex-col items-center justify-center gap-8">
+    <section className="flex min-h-[100dvh] flex-col items-center justify-center gap-8 py-16 md:py-24">
       <h1 className="handwritten text-4xl font-semibold uppercase">
         Next TODO
       </h1>
