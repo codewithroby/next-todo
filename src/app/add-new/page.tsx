7050a7961a7818e6ba53fcc81496ea02aa759119
Roby Codes <robert.cfx@proton.me>
2024-09-15 23:24:41 +0200

    feat(form): Created add-todo form.

--- src/app/add-new/page.tsx
+++ src/app/add-new/page.tsx
@@ -1,7 +1,16 @@
+import { AddTodoForm } from "~/components/ui/todo/add-form";
+
 const AddNewTodoPage = () => (
   <main>
-    <section className="flex min-h-screen items-center justify-center">
-      <h1 className="handwritten text-5xl font-bold uppercase">Add New Todo</h1>
+    <section className="flex min-h-screen flex-col items-center justify-center gap-6">
+      <h1 className="handwritten text-4xl font-semibold uppercase">
+        Add New Todo
+      </h1>
+      <div className="container max-w-xl">
+        <div className="rounded-md bg-white p-8 shadow-sm">
+          <AddTodoForm />
+        </div>
+      </div>
     </section>
   </main>
 );
