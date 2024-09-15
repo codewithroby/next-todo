a885e57b1b5337c0e57f0522f6bab7894fe0f3f9
Roby Codes <robert.cfx@proton.me>
2024-09-15 23:24:29 +0200

    feat(form): Created add-todo form.

---
+++ src/components/ui/todo/add-form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { addTodo } from "~/actions/todo-crud";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { addTodoFormSchema, AddTodoFormSchemaType } from "~/lib/types";
+import { useRouter } from "next/router";
+import { revalidatePath } from "next/cache";
+
+const AddTodoForm = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<AddTodoFormSchemaType>>({
+    resolver: zodResolver(addTodoFormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<AddTodoFormSchemaType>) {
+    await addTodo(values).then(() => {
+      router.push("/");
+      revalidatePath("/");
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Homework" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Writing the essay for tomorrow's exam."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add New Todo</Button>
+      </form>
+    </Form>
+  );
+};
+
+export { AddTodoForm };
