ba99f1d1e9b6a4295b5eb95d9de0180ea2660682
Roby Codes <robert.cfx@proton.me>
2024-09-18 00:12:18 +0200

    fix(total-todos-counter): fixed component receiving input data of a wrong type.

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link";
+import * as todos from "~/actions/todo-crud";
 import { LINKS } from "~/lib/links";
 import { TotalTodosCounter } from "~/components/ui/todo/total-todos-counter";
 
-const Header = () => (
-  <header className="relative flex justify-center">
-    <nav className="fixed top-4 z-10 flex">
-      <ul className="flex items-center justify-center gap-4 rounded-full bg-white/75 py-2 pl-4 pr-2 shadow-sm backdrop-blur-sm">
-        {LINKS.map((link, index) => (
-          <li key={index} className="text-sm uppercase">
-            <Link href={link.href} aria-label={link.title}>
-              {link.title}
-            </Link>
+const Header = async () => {
+  const initialTotalTodos = await todos.getTotal().then((data) => {
+    return data[0].count;
+  });
+
+  return (
+    <header className="relative flex justify-center">
+      <nav className="fixed top-4 z-10 flex">
+        <ul className="flex items-center justify-center gap-4 rounded-full bg-white/75 py-2 pl-4 pr-2 shadow-sm backdrop-blur-sm">
+          {LINKS.map((link, index) => (
+            <li key={index} className="text-sm uppercase">
+              <Link href={link.href} aria-label={link.title}>
+                {link.title}
+              </Link>
+            </li>
+          ))}
+          <li className="rounded-full bg-black px-2 py-1 text-xs uppercase text-white">
+            Total Todos :{" "}
+            <TotalTodosCounter initialTotalTodos={initialTotalTodos} />
           </li>
-        ))}
-        <li className="rounded-full bg-black px-2 py-1 text-xs uppercase text-white">
-          Total Todos : <TotalTodosCounter />
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export { Header };
