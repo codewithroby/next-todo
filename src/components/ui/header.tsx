ee630451e6197de5d625319afe9e589f2fe1c9d0
Roby Codes <robert.cfx@proton.me>
2024-09-18 00:01:53 +0200

    style(header): Added total todos count component to header.

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { LINKS } from "~/lib/links";
+import { TotalTodosCounter } from "~/components/ui/todo/total-todos-counter";
 
 const Header = () => (
   <header className="relative flex justify-center">
-    <nav className="fixed top-4 z-10 flex items-center justify-center">
-      <ul className="flex gap-4 rounded-full bg-white/75 px-4 py-2 shadow-sm backdrop-blur-sm">
+    <nav className="fixed top-4 z-10 flex">
+      <ul className="flex items-center justify-center gap-4 rounded-full bg-white/75 py-2 pl-4 pr-2 shadow-sm backdrop-blur-sm">
         {LINKS.map((link, index) => (
           <li key={index} className="text-sm uppercase">
             <Link href={link.href} aria-label={link.title}>
@@ -12,6 +13,9 @@ const Header = () => (
             </Link>
           </li>
         ))}
+        <li className="rounded-full bg-black px-2 py-1 text-xs uppercase text-white">
+          Total Todos : <TotalTodosCounter />
+        </li>
       </ul>
     </nav>
   </header>
