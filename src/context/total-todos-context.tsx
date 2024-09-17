22b801e6f53a2251fd1ed275b0b56dfb3699a75a
Roby Codes <robert.cfx@proton.me>
2024-09-17 23:52:40 +0200

    feat(total-todos-contest): Created context containing total todos for pagination ui creation.

---
+++ src/context/total-todos-context.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+import { TodoContextType } from "~/types/todo-context";
+import * as todos from "~/actions/todo-crud";
+
+const TotalTodosContext = createContext<TodoContextType | null>(null);
+
+const TotalTodosContextProvider = async ({ children }: ReactNode) => {
+  const [totalTodos, setTotalTodos] = useState<number>(0);
+
+  await todos.getTotal().then((data) => {
+    setTotalTodos(data[0].count);
+  });
+
+  return (
+    <TotalTodosContext.Provider
+      value={{ totalTodos: totalTodos, setTotalTodos: setTotalTodos }}
+    >
+      {children}
+    </TotalTodosContext.Provider>
+  );
+};
+
+const useTotalTodosContext = () => {
+  const totalTodosContext = useContext(TotalTodosContext);
+
+  if (totalTodosContext === null) {
+    throw new Error(
+      "useTotalTodosContext must be used from within a TotalTodosContextProvider.",
+    );
+  }
+
+  return totalTodosContext;
+};
