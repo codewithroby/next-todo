539ff8dce7a4a22f044020d5747c96b5703fd0cb
Roby Codes <robert.cfx@proton.me>
2024-09-17 23:49:22 +0200

    feat(type): Created todo-context type file.

--- src/types/todo-context.ts
+++ src/types/todo-context.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 type TodoContextType = {
   totalTodos: number;
-  setTotalTodos: React.Dispatch<React.SetStateAction<boolean>>;
+  setTotalTodos: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export type { TodoContextType };
