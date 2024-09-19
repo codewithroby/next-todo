021c5506f1ed8911cd7914956c7541696f05d4f8
Roby Codes <robert.cfx@proton.me>
2024-09-19 17:09:24 +0200

    syle(arrow button): Improved UX by disabling arrow buttons when reaching first or last page.

--- src/components/ui/todo/pagination.tsx
+++ src/components/ui/todo/pagination.tsx
@@ -9,26 +9,30 @@ const TodoPagination = ({ page, total }: { page: number; total: number }) => {
   const router = useRouter();
 
   const handlePreviousPage = () => {
-    if (page > 0) {
-      router.replace(`/?page=${page - 1}`);
-    }
+    router.replace(`/?page=${page - 1}`);
   };
 
   const handleNextPage = () => {
-    if (page < totalPages) {
-      router.replace(`/?page=${page + 1}`);
-    }
+    router.replace(`/?page=${page + 1}`);
   };
 
   return (
     <div className="flex items-center justify-center space-x-2">
-      <Button className="size-6 rounded-full p-0" onClick={handlePreviousPage}>
+      <Button
+        className="size-6 rounded-full p-0"
+        onClick={handlePreviousPage}
+        disabled={page <= 1}
+      >
         <IoArrowBack className="size-4" />
       </Button>
       <span>
         {page} / {totalPages}
       </span>
-      <Button className="size-6 rounded-full p-0" onClick={handleNextPage}>
+      <Button
+        className="size-6 rounded-full p-0"
+        onClick={handleNextPage}
+        disabled={page >= totalPages}
+      >
         <IoArrowForward />
       </Button>
     </div>
