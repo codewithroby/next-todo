be21015f4413724d2459a4ff5084b4980909c6af
Roby Codes <robert.cfx@proton.me>
2024-09-28 16:18:30 +0200

    config(env): Removed unused variable.

--- src/env.js
+++ src/env.js
@@ -6,14 +6,10 @@ export const env = createEnv({
     DOMAIN: z.string().url(),
     DATABASE_URL: z.string().url(),
   },
-  client: {
-    NEXT_PUBLIC_TEST: z.string().min(1),
-  },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DOMAIN: process.env.DOMAIN,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_TEST: process.env.NEXT_PUBLIC_TEST,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
