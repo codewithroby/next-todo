c2474dda228fc4e9cf0afba651f14cec29017ac1
Roby Codes <robert.cfx@proton.me>
2024-09-29 22:59:16 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ const HomePage = ({
         </Suspense>
         <Suspense
           fallback={<TodoPaginationSkeleton />}
-          key="PaginationSuspense"
+          key={`pagination${page}`}
         >
           <PaginationWrapper page={page} />
         </Suspense>
